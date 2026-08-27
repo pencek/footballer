c3688fdc62a698f018be64645dbf3f7580d2e02a
1 <869244169@qq.com>
2026-08-27 20:33:00 +0800

    主页

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://tkomfsasuwvvkffxlmtk.supabase.co";
-const SUPABASE_KEY = "sb_publishable_77zkLllta4fd0-dz1ksCnw_yDwViGLj";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
